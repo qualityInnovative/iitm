9cf6f328b6dd7c8abd6b8bab6708cc05bb2db8ba
ubaidh <obaidh@live.com>
2025-06-16 18:16:03 +0530

    eewq

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -14,7 +14,7 @@ const showNavLink = (node) => {
       .classList.remove("hide-nav-links");
   else if (node.textContent.toLowerCase() == "campus")
     document.querySelector("#campus-links").classList.remove("hide-nav-links");
-  else if (node.textContent.toLowerCase() == "Admissions(new)")
+  else if (node.textContent.toLowerCase() == "admissions(new)")
     document
       .querySelector("#admissions-links")
       .classList.remove("hide-nav-links");
